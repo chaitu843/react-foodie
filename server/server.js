600ef4f0fb15629756e67682f52ad8dd8f73c328
chaitu843 <35913635+chaitu843@users.noreply.github.com>
2019-07-24 19:09:45 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ app.use(express.static('build'));
 
 app.use(express.json());
 
-const port = process.env.port || 8080;
+const port = process.env.port || 5000;
 const FavoriteCollectionRoutes = require('./Routes/FavoriteCollectionRoutes');
 const FavoriteRestaurantRoutes = require('./Routes/FavoriteRestaurantRoutes');
 
